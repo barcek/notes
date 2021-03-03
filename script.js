bd61374c27e221ff6926c9d0158efe08625accf6
barcek <barcek@gmx.com>
2021-03-03 11:11:19 +0000

    feature: added setting save & load logic & applied for theme

--- script.js
+++ script.js
@@ -2,11 +2,47 @@
 
     'use strict';
 
+    /* Utilities */
+
+    var saveLocal = function(key, value) {
+        if (w.localStorage) {
+            w.localStorage.setItem(key, JSON.stringify(value));
+        };
+    };
+
+    var loadLocal = function(key) {
+        if (w.localStorage && w.localStorage.getItem(key)) {
+            return JSON.parse(w.localStorage.getItem(key));
+        };
+    };
+
+    var saveSetting = function(key, value) {
+        settingsObject[key] = value;
+        saveLocal('Settings', settingsObject);
+    };
+
+    var loadSetting = function(key) {
+        return loadLocal("Settings")[key];
+    };
+
+    /* Elements */
+
     var themeToggle = d.querySelector('header > .theme-toggle');
 
+    /* Constants */
+
+    var darkThemeClass = 'theme--dark';
+    var settingsObject = { theme: null };
+
+    /* Handlers */
+
     function handleThemeToggle() {
-        d.body.classList.toggle('theme--dark');
+        d.body.classList.toggle(darkThemeClass);
+        var activeTheme = d.body.classList.contains(darkThemeClass) ? "dark" : "light";
+        saveSetting('theme', activeTheme);
     };
+
+    /* Listeners */
 
     themeToggle.addEventListener('click', handleThemeToggle);
     themeToggle.addEventListener('keydown', function(e) {
@@ -16,5 +52,11 @@
         };
     });
 
+    /* Initializers */
+
+    // if dark theme currently set, use dark
+    if (loadSetting('theme') && loadSetting('theme') === "dark") {
+        d.body.classList.add(darkThemeClass);
+    };
 
 })(window, document)
